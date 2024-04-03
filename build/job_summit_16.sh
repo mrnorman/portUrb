05bb7719eceee590515a9c204280bdf9d2f4e55e
Matt Norman <normanmr@ornl.gov>
2024-04-03 11:11:40 -0400

    build/flop_efficiency.txt

---
+++ build/job_summit_16.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#BSUB -P STF006
+#BSUB -W 0:20
+#BSUB -nnodes 16
+#BSUB -J porturb
+#BSUB -o porturb.%J
+#BSUB -e porturb.%J
+
+num_rs=`echo "($LSB_DJOB_NUMPROC-1)/42*6" | bc`
+num_nodes=`echo "($LSB_DJOB_NUMPROC-1)/42" | bc`
+builddir=/gpfs/alpine2/stf006/scratch/imn/portUrb/build
+mydir=$builddir/summit/nodes_$num_nodes
+mkdir -p $mydir
+cd $mydir
+cp $builddir/wind_farm .
+cp $builddir/inputs/input_windfarm_$num_nodes.yaml .
+mkdir -p inputs
+cp $builddir/inputs/IEA-15-240-RWT.yaml ./inputs
+source $builddir/machines/summit/summit_gpu.env
+jsrun --smpiargs="-gpu" -n $num_rs -a 1 -c 1 -g 1 ./wind_farm ./input_windfarm_$num_nodes.yaml >& portUrb.out
+
--- build/timer_awk_script.sh
+++ build/timer_awk_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-nodes=16  && grep "\<$1\>" frontier/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
-nodes=32  && grep "\<$1\>" frontier/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
-nodes=64  && grep "\<$1\>" frontier/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
-nodes=128 && grep "\<$1\>" frontier/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
-nodes=256 && grep "\<$1\>" frontier/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
-nodes=512 && grep "\<$1\>" frontier/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
+machine=summit
+
+nodes=16  && grep "\<$1\>" $machine/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
+nodes=32  && grep "\<$1\>" $machine/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
+nodes=64  && grep "\<$1\>" $machine/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
+nodes=128 && grep "\<$1\>" $machine/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
+nodes=256 && grep "\<$1\>" $machine/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
+nodes=512 && grep "\<$1\>" $machine/nodes_$nodes/portUrb.out | awk 'BEGIN{t=0;c=0;mn=1e6;mx=0}{t+=$3;c++;mn=$3<mn?$3:mn;mx=$3>mx?$3:mx}END{print t/c "\n" mn "\n" mx}'
 

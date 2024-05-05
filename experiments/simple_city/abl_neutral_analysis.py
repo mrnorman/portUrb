49a9aa44e660f2d48a74a6ac682459b7e0477e14
Matt Norman <normanmr@ornl.gov>
2024-05-05 13:44:33 -0400

    Committing changes to analysis script

--- experiments/simple_city/abl_neutral_analysis.py
+++ experiments/simple_city/abl_neutral_analysis.py
@@ -2,6 +2,9 @@ from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import colormaps as cmaps
+
+plt.rcParams.update({'font.size': 10})
 
 
 def spectra(T,binfact,maxbinsize,dx = 1) :
@@ -41,6 +44,9 @@ def spectra(T,binfact,maxbinsize,dx = 1) :
     binsize *= binfact
     ind += inc
     count = count + 1
+  freq_bins[count-2] = np.mean(freq_bins[count-2:count])
+  spd_bins [count-2] = np.mean(spd_bins [count-2:count])
+  count -= 1
 
   return freq_bins[:count]*2*2*np.pi/(2*dx) , spd_bins[:count]
 
@@ -54,74 +60,70 @@ def mams_plot(z,v,t,f,lgy=False) :
   plt.fill_betweenx(z,ua-us,ua+us,color="deepskyblue")
   plt.plot(ua,z,color="black")
   plt.margins(x=0)
-  plt.tight_layout()
   plt.xlabel(t)
   plt.ylabel("z-location (km)")
   if (lgy) :
     plt.yscale("log")
-  plt.savefig(f,dpi=300)
+  plt.savefig(f,dpi=300,bbox_inches="tight")
   plt.show()
   plt.close()
 
 
-nc1 = Dataset("abl_neutral_10m_00000032.nc","r")
-nc2 = Dataset("abl_neutral_10m_00000040.nc","r")
-nz = nc1.dimensions["z"].size
-ny = nc1.dimensions["y"].size
-nx = nc1.dimensions["x"].size
-x = nc1.variables["x"][:]/1000.
-y = nc1.variables["y"][:]/1000.
-z = nc1.variables["z"][:]/1000.
+nc = Dataset("abl_neutral_10m_00000040.nc","r")
+nz = nc.dimensions["z"].size
+ny = nc.dimensions["y"].size
+nx = nc.dimensions["x"].size
+x = nc.variables["x"][:]/1000.
+y = nc.variables["y"][:]/1000.
+z = nc.variables["z"][:]/1000.
+mycmap = cmaps.vik
+num_levels = 256
 
 yind = int(ny/2)
 z2   = int(750./1200.*nz)
-u = nc2.variables["uvel"][:z2,yind,:,0]
-v = nc2.variables["vvel"][:z2,yind,:,0]
-w = nc2.variables["wvel"][:z2,yind,:,0]
+u = nc.variables["uvel"][:z2,yind,:,0]
+v = nc.variables["vvel"][:z2,yind,:,0]
+w = nc.variables["wvel"][:z2,yind,:,0]
 mag = np.sqrt(u*u+v*v+w*w)
 X,Z = np.meshgrid(x,z[:z2])
-CS = plt.contourf(X,Z,mag,levels=100,cmap="turbo",extend="both")
-plt.title(f"Wind Speed at y={y[yind]}km")
+CS = plt.contourf(X,Z,mag,levels=num_levels,cmap=mycmap,extend="both")
 plt.axis('scaled')
 plt.margins(x=0)
-plt.tight_layout()
 plt.xlabel("x-location (km)")
 plt.ylabel("z-location (km)")
 divider = make_axes_locatable(plt.gca())
 cax = divider.append_axes("bottom", size="5%", pad=0.5)
 cbar = plt.colorbar(CS,orientation="horizontal",cax=cax)
-cbar.ax.tick_params(rotation=45)
-plt.savefig("abl_neutral_wind_countour_xz.png",dpi=300)
+cbar.ax.tick_params(rotation=25)
+plt.savefig("abl_neutral_wind_countour_xz.png",dpi=300,bbox_inches="tight")
 plt.show()
 plt.close()
 
 zind = int(80./1200.*nz)
-u = nc2.variables["uvel"][zind,:,:,0]
-v = nc2.variables["vvel"][zind,:,:,0]
-w = nc2.variables["wvel"][zind,:,:,0]
+u = nc.variables["uvel"][zind,:,:,0]
+v = nc.variables["vvel"][zind,:,:,0]
+w = nc.variables["wvel"][zind,:,:,0]
 mag = np.sqrt(u*u+v*v+w*w)
 X,Y = np.meshgrid(x,y)
-CS = plt.contourf(X,Y,mag,levels=100,cmap="turbo",extend="both")
-plt.title(f"Wind Speed at y={y[yind]}km")
+CS = plt.contourf(X,Y,mag,levels=num_levels,cmap=mycmap,extend="both")
 plt.axis('scaled')
 plt.margins(x=0)
-plt.tight_layout()
 plt.xlabel("x-location (km)")
 plt.ylabel("y-location (km)")
 divider = make_axes_locatable(plt.gca())
 cax = divider.append_axes("bottom", size="5%", pad=0.5)
 cbar = plt.colorbar(CS,orientation="horizontal",cax=cax)
-cbar.ax.tick_params(rotation=45)
-plt.savefig("abl_neutral_wind_countour_xy.png",dpi=300)
+cbar.ax.tick_params(rotation=25)
+plt.savefig("abl_neutral_wind_countour_xy.png",dpi=300,bbox_inches="tight")
 plt.show()
 plt.close()
 
 
 z2 = int(1000./1200.*nz)
-u     = nc2.variables["uvel"][:z2,:,:,0]
-v     = nc2.variables["vvel"][:z2,:,:,0]
-w     = nc2.variables["wvel"][:z2,:,:,0]
-tke_u = nc2.variables["TKE" ][:z2,:,:,0]
+u     = nc.variables["uvel"][:z2,:,:,0]
+v     = nc.variables["vvel"][:z2,:,:,0]
+w     = nc.variables["wvel"][:z2,:,:,0]
+tke_u = nc.variables["TKE" ][:z2,:,:,0]
 mag = np.sqrt(u*u+v*v+w*w)
 up = np.subtract( u , np.mean(u,axis=(1,2))[:,np.newaxis,np.newaxis] )
 vp = np.subtract( v , np.mean(v,axis=(1,2))[:,np.newaxis,np.newaxis] )
@@ -139,15 +141,15 @@ mams_plot(z[:z2],tke_u,'Uesolved TKE'    ,'abl_neutral_tke_u.png')
 dx = 10
 z1 = int(100./1200.*nz)
 z2 = int(400./1200.*nz)
-u  = nc2.variables["uvel"][z1:z2,:,:,0]
-v  = nc2.variables["vvel"][z1:z2,:,:,0]
-w  = nc2.variables["wvel"][z1:z2,:,:,0]
-freq,spd = spectra((u*u+v*v+w*w)/2,1.02,50,dx)
+u  = nc.variables["uvel"][z1:z2,:,:,0]
+v  = nc.variables["vvel"][z1:z2,:,:,0]
+w  = nc.variables["wvel"][z1:z2,:,:,0]
+freq,spd = spectra((u*u+v*v+w*w)/2,1.03,50,dx)
 plt.loglog( freq , spd*freq**(5/3) , label="KE")
-plt.xlabel(r"$Frequency$")
+plt.xlabel(r"Frequency")
 plt.ylabel(r"$E(K) \omega^{5/3}$ (Compensated KE Spectra) ")
 plt.legend()
-plt.savefig("abl_neutral_ke_spectra.png",dpi=300)
+plt.savefig("abl_neutral_ke_spectra.png",dpi=300,bbox_inches="tight")
 plt.show()
 plt.close()
 

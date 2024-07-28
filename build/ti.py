ab38fbeabc9d46c2769a0836310d424f4c77a4ba
Matt Norman <normanmr@ornl.gov>
2024-07-28 17:37:48 -0400

    Updating python script to be a bit smarter

--- build/ti.py
+++ build/ti.py
@@ -2,122 +2,53 @@ from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 import numpy as np
 
-iend = 10
+def get_ind(arr,val) :
+    return np.argmin(np.abs(arr-val))
 
-for i in range(1,iend) :
-    nc  = Dataset("z0_0.3/validation_precursor_000000"+str(i).zfill(2)+".nc","r")
-    nz = nc.dimensions["z"].size
-    u = nc.variables["uvel"][:,:,:]
-    v = nc.variables["vvel"][:,:,:]
-    z = nc.variables["z"][:]
-    mag = np.sqrt(u*u + v*v)
-    tot  = 0
+def turbulent_intensity(fname) :
+    nc    = Dataset(fname,"r")
+    nz    = nc.dimensions["z"].size
+    u     = nc.variables["uvel"][:,:,:]
+    v     = nc.variables["vvel"][:,:,:]
+    z     = nc.variables["z"][:]
+    k1    = get_ind(z,89-127/2)
+    k2    = get_ind(z,89+127/2)
+    mag   = np.sqrt(u*u + v*v)
+    tot   = 0
     count = 0
-    for k in range(2,16) :
+    for k in range(k1,k2+1) :
         tot  += np.std(mag [k,:,:])/np.mean(mag [k,:,:])
         count = count + 1
-    ti = tot / count
-    print(ti,end=" , ")
+    return tot / count
 
-    nc  = Dataset("z0_0.1/validation_precursor_000000"+str(i).zfill(2)+".nc","r")
-    nz = nc.dimensions["z"].size
-    u = nc.variables["uvel"][:,:,:]
-    v = nc.variables["vvel"][:,:,:]
-    z = nc.variables["z"][:]
-    mag = np.sqrt(u*u + v*v)
-    tot  = 0
-    count = 0
-    for k in range(2,16) :
-        tot  += np.std(mag [k,:,:])/np.mean(mag [k,:,:])
-        count = count + 1
-    ti = tot / count
-    print(ti,end=" , ")
+def shear_exponent(fname) :
+    nc    = Dataset(fname,"r")
+    nz    = nc.dimensions["z"].size
+    z     = nc.variables["z"][:]
+    k1    = get_ind(z,89-127/2)
+    k2    = get_ind(z,89+127/2)
+    u     = nc.variables["avg_u"][k1,:,:]
+    v     = nc.variables["avg_v"][k1,:,:]
+    mag1  = np.mean(np.sqrt(u*u + v*v))
+    u     = nc.variables["avg_u"][k2,:,:]
+    v     = nc.variables["avg_v"][k2,:,:]
+    mag2  = np.mean(np.sqrt(u*u + v*v))
+    return np.log(mag2/mag1)/np.log(z[k2]/z[k1])
 
-    nc  = Dataset("z0_0.07/validation_precursor_000000"+str(i).zfill(2)+".nc","r")
-    nz = nc.dimensions["z"].size
-    u = nc.variables["uvel"][:,:,:]
-    v = nc.variables["vvel"][:,:,:]
-    z = nc.variables["z"][:]
-    mag = np.sqrt(u*u + v*v)
-    tot  = 0
-    count = 0
-    for k in range(2,16) :
-        tot  += np.std(mag [k,:,:])/np.mean(mag [k,:,:])
-        count = count + 1
-    ti = tot / count
-    print(ti,end=" , ")
+iend = 24
 
-    nc  = Dataset("validation_precursor_000000"+str(i).zfill(2)+".nc","r")
-    nz = nc.dimensions["z"].size
-    u = nc.variables["uvel"][:,:,:]
-    v = nc.variables["vvel"][:,:,:]
-    z = nc.variables["z"][:]
-    mag = np.sqrt(u*u + v*v)
-    tot  = 0
-    count = 0
-    for k in range(2,16) :
-        tot  += np.std(mag [k,:,:])/np.mean(mag [k,:,:])
-        count = count + 1
-    ti = tot / count
-    print(ti)
+for i in range(1,iend+1) :
+    print(turbulent_intensity( "z0_0.3/validation_precursor_000000"+str(i).zfill(2)+".nc"),end=" , ")
+    print(turbulent_intensity( "z0_0.1/validation_precursor_000000"+str(i).zfill(2)+".nc"),end=" , ")
+    print(turbulent_intensity("z0_0.07/validation_precursor_000000"+str(i).zfill(2)+".nc"),end=" , ")
+    print(turbulent_intensity("z0_0.05/validation_precursor_000000"+str(i).zfill(2)+".nc")          )
 
 print("")
 
-for i in range(1,iend) :
-    nc  = Dataset("z0_0.3/validation_precursor_000000"+str(i).zfill(2)+".nc","r")
-    nz = nc.dimensions["z"].size
-    z = nc.variables["z"][:]
-    u = nc.variables["avg_u"][2,:,:]
-    v = nc.variables["avg_v"][2,:,:]
-    mag1 = np.mean(np.sqrt(u*u + v*v))
-    u = nc.variables["avg_u"][15,:,:]
-    v = nc.variables["avg_v"][15,:,:]
-    mag2 = np.mean(np.sqrt(u*u + v*v))
-    z1 = z[ 2]
-    z2 = z[15]
-    p = np.log(mag2/mag1)/np.log(z2/z1)
-    print(p,end=" , ")
-
-    nc  = Dataset("z0_0.1/validation_precursor_000000"+str(i).zfill(2)+".nc","r")
-    nz = nc.dimensions["z"].size
-    z = nc.variables["z"][:]
-    u = nc.variables["avg_u"][2,:,:]
-    v = nc.variables["avg_v"][2,:,:]
-    mag1 = np.mean(np.sqrt(u*u + v*v))
-    u = nc.variables["avg_u"][15,:,:]
-    v = nc.variables["avg_v"][15,:,:]
-    mag2 = np.mean(np.sqrt(u*u + v*v))
-    z1 = z[ 2]
-    z2 = z[15]
-    p = np.log(mag2/mag1)/np.log(z2/z1)
-    print(p,end=" , ")
-
-    nc  = Dataset("z0_0.07/validation_precursor_000000"+str(i).zfill(2)+".nc","r")
-    nz = nc.dimensions["z"].size
-    z = nc.variables["z"][:]
-    u = nc.variables["avg_u"][2,:,:]
-    v = nc.variables["avg_v"][2,:,:]
-    mag1 = np.mean(np.sqrt(u*u + v*v))
-    u = nc.variables["avg_u"][15,:,:]
-    v = nc.variables["avg_v"][15,:,:]
-    mag2 = np.mean(np.sqrt(u*u + v*v))
-    z1 = z[ 2]
-    z2 = z[15]
-    p = np.log(mag2/mag1)/np.log(z2/z1)
-    print(p,end=" , ")
-
-    nc  = Dataset("validation_precursor_000000"+str(i).zfill(2)+".nc","r")
-    nz = nc.dimensions["z"].size
-    z = nc.variables["z"][:]
-    u = nc.variables["avg_u"][2,:,:]
-    v = nc.variables["avg_v"][2,:,:]
-    mag1 = np.mean(np.sqrt(u*u + v*v))
-    u = nc.variables["avg_u"][15,:,:]
-    v = nc.variables["avg_v"][15,:,:]
-    mag2 = np.mean(np.sqrt(u*u + v*v))
-    z1 = z[ 2]
-    z2 = z[15]
-    p = np.log(mag2/mag1)/np.log(z2/z1)
-    print(p)
+for i in range(1,iend+1) :
+    print(shear_exponent( "z0_0.3/validation_precursor_000000"+str(i).zfill(2)+".nc"),end=" , ")
+    print(shear_exponent( "z0_0.1/validation_precursor_000000"+str(i).zfill(2)+".nc"),end=" , ")
+    print(shear_exponent("z0_0.07/validation_precursor_000000"+str(i).zfill(2)+".nc"),end=" , ")
+    print(shear_exponent("z0_0.05/validation_precursor_000000"+str(i).zfill(2)+".nc")          )
 
 

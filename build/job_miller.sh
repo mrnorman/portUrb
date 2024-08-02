316021929aff2264263d0aaf59772fe638992a6b
Matt Norman <normanmr@ornl.gov>
2024-08-02 09:44:38 -0400

    Adding miller files

---
+++ build/job_miller.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --cluster fawbush
+#SBATCH --partition ampere
+#SBATCH --cluster-constraint=blue
+#SBATCH --exclusive
+#SBATCH -A nwp501
+#SBATCH -J portUrb
+#SBATCH -o %x-%j.out
+#SBATCH -t 8:00:00
+#SBATCH -N 30
+
+num_tasks=`echo "$SLURM_NNODES*4" | bc`
+cd /lustre/storm/nwp501/scratch/imn/portUrb/build
+source machines/miller/miller_gpu.env
+srun -N $SLURM_NNODES -n $num_tasks -c 32 --gpus-per-task=1 --gpu-bind=closest ./wind_farm ./inputs/input_windfarm.yaml >& portUrb.out
+

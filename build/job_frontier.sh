df8f348b968e72b92137f5501ae96a22b92d4caf
Matt Norman <normanmr@ornl.gov>
2024-09-05 14:19:15 -0400

    Adding Stable ABL analysis script

--- build/job_frontier.sh
+++ build/job_frontier.sh
@@ -2,13 +2,13 @@
 #SBATCH -A stf006
 #SBATCH -J portUrb
 #SBATCH -o %x-%j.out
-#SBATCH -t 24:00:00
+#SBATCH -t 12:00:00
 #SBATCH --partition extended
-#SBATCH -N 35
+#SBATCH -N 16
 
 num_tasks=`echo "$SLURM_JOB_NUM_NODES*8" | bc`
 cd /lustre/orion/stf006/scratch/imn/portUrb/build
 source machines/frontier/frontier_gpu.env
-srun -n $num_tasks -c 1 --gpus-per-task=1 --gpu-bind=closest ./windfarm_ensemble
+srun -n $num_tasks -c 1 --gpus-per-task=1 --gpu-bind=closest ./abl ./inputs/input_abl_stable.yaml
 # srun -n $num_tasks -c 1 --gpus-per-task=1 --gpu-bind=closest ./wind_farm2 ./inputs/input_windfarm2.yaml
 

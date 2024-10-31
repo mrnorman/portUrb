bbec7cf38f67ee138cf77ad798d7a92fb948989d
Matt Norman <normanmr@ornl.gov>
2024-10-31 08:39:26 -0400

    Reorganizing and cleaning up a bit

--- build/cmakeclean.sh
+++ build/cmakeclean.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[[ -f Makefile ]] && make clean
+
 rm -rf CMakeCache.txt  CMakeFiles  cmake_install.cmake  CTestTestfile.cmake  Makefile  yakl  \
-       core  modules  custom_modules  inputs  model  libcustom_modules.a
+       core  modules  custom_modules  inputs  model  libcustom_modules.a  cmake_packages     \
+       generated  git-state.txt
 
